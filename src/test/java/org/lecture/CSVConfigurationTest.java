9ad0ae8946381ddf9a161c4aa00dcc8039f574e6
micha <2010859011@fh-burgenland.at>
2021-03-27 19:44:59 +0100

    new tests added

--- src/test/java/org/lecture/CSVConfigurationTest.java
+++ src/test/java/org/lecture/CSVConfigurationTest.java
@@ -7,8 +7,6 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CSVConfigurationTest {
--- src/test/java/org/lecture/WindSpeedEntryTest.java
+++ src/test/java/org/lecture/WindSpeedEntryTest.java
@@ -1,11 +1,77 @@
 package org.lecture;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class WindSpeedEntryTest {
 
     @Test
-    public void windSpeedEntryBuilderTest() {
+    public void testExtreme() {
 
+        WindSpeedEntry extremeHurricane = new WindSpeedEntry.WindSpeedEntryBuilder()
+                .withId(1)
+                .withWindSpeed((double) 120)
+                .withDate("26.01.2020 18:35:20")
+                .withStation("Istanbul")
+                .withTemperature((double) 30)
+                .withWindDirection("NE")
+                .withWeatherCondition("cloudy")
+                .build();
+
+        WindSpeedEntry extremeCalm = new WindSpeedEntry.WindSpeedEntryBuilder()
+                .withId(1)
+                .withWindSpeed((double) 1)
+                .withDate("26.01.2020 18:35:20")
+                .withStation("Istanbul")
+                .withTemperature((double) 30)
+                .withWindDirection("NE")
+                .withWeatherCondition("cloudy")
+                .build();
+
+        String extremeHurricaneTest = extremeHurricane.getExtreme();
+        String extremeCalmTest = extremeCalm.getExtreme();
+        String extremeHurricaneTestExp = "hurricane";
+        String extremeCalmTestExp = "calm";
+
+        Assertions.assertEquals(extremeHurricaneTestExp,extremeHurricaneTest);
+        Assertions.assertEquals(extremeCalmTestExp,extremeCalmTest);
+    }
+
+    @Test
+    public void testBeaufort() {
+
+        WindSpeedEntry beaufortTest = new WindSpeedEntry.WindSpeedEntryBuilder()
+                .withId(1)
+                .withWindSpeed((double) 120)
+                .withDate("26.01.2020 18:35:20")
+                .withStation("Istanbul")
+                .withTemperature((double) 30)
+                .withWindDirection("NE")
+                .withWeatherCondition("cloudy")
+                .build();
+
+        Double beaufortTestResult = beaufortTest.getBeaufort();
+        Double beaufortExp = 13.959039999999998;
+
+        Assertions.assertEquals(beaufortExp,beaufortTestResult);
+    }
+
+    @Test
+    public void testKnots() {
+
+        WindSpeedEntry knotsTest = new WindSpeedEntry.WindSpeedEntryBuilder()
+                .withId(1)
+                .withWindSpeed((double) 120)
+                .withDate("26.01.2020 18:35:20")
+                .withStation("Istanbul")
+                .withTemperature((double) 30)
+                .withWindDirection("NE")
+                .withWeatherCondition("cloudy")
+                .build();
+
+        Double knotsTestResult = knotsTest.getKnots();
+        Double knotsExp = 5.3996;
+
+        Assertions.assertEquals(knotsExp,knotsTestResult);
     }
 }
